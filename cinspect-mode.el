1a686ce67678278fe03ad12b487997ba02d2fce4
byels <byelsey1@gmail.com>
2015-07-08 18:31:56 -0400

    Remove markdown-style links from .el readme

--- cinspect-mode.el
+++ cinspect-mode.el
@@ -11,8 +11,8 @@
 
 ;;; Commentary:
 
-;; Use [cinspect](https://github.com/punchagan/cinspect) to look at the CPython source of builtins and other C objects!
-;; `cinspect-mode` can and optimally should be used in concert with [Jedi.el](http://tkf.github.io/emacs-jedi).
+;; Use cinspect to look at the CPython source of builtins and other C objects!
+;; `cinspect-mode` can and optimally should be used in concert with Jedi.el.
 
 ;;; Code:
 
